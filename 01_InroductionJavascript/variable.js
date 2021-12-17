68b2d11cae3f483eddff6a460c2dbe8c35331651
Amar Nath <nthvpsn@gmail.com>
2021-12-17 23:55:22 +0530

    Update variable file

--- 01_InroductionJavascript/variable.js
+++ 01_InroductionJavascript/variable.js
@@ -41,3 +41,24 @@ console.log(c); //undefined
 
 const fixed = 5;
 console.log(fixed);
+
+/********************************************/
+//FUNCTION
+function addition(a, b) {
+    console.log("Sum :- " + (a + b));
+}
+addition(3, 4);
+
+// function as first class citizens
+const multi = function (a, b) {
+    return a * b;
+}
+const result = multi(4, 5);
+console.log(result);
+
+
+//IIFE(immediately invoked function Expression)
+let add = (function (a, b) {
+    return a + b;
+})(10, 20);
+console.log(add);
